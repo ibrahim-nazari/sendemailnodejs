09b1b2829f13ab080916066d3e3f964b969dbbdd
ibrahim nazari <ibrahimnazaryweb@gmail.com>
2021-09-16 14:49:56 +0430

    error fixed

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.post("/send",
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ message: errors.array(),error:true });
     }
     const {subject,name,phone,email,message}=req.body
     
