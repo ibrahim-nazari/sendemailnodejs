347cf56ea4acbb0f8aa0ee911753c0fb6e43620b
ibrahim nazari <ibrahimnazaryweb@gmail.com>
2021-09-16 19:43:16 +0430

    update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post("/send",
   let transporter = nodemailer.createTransport({
     host: process.env.SMTP,
     port: 587,
-    secure: true, 
+    secure: false, 
     auth: {
       user: process.env.USER_EMAIL, 
       pass: process.env.USER_PASSWORD,
